561096ddc145401f8be8222910d2168f66b4f5fc
Elif Özkan <64463656+elifcozkan@users.noreply.github.com>
2024-08-23 00:02:12 +0300

    Update source.h

--- project3/source.h
+++ project3/source.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <ctype.h>
 
 // Macro for nonused return values
 #define NU (void) 
@@ -11,5 +12,18 @@
 
 void freeStandardInputBuffer(void);
 
-void stringGets(char* ch);
+void stringGets(char* p);
 
+int stringLength(char* ch);
+
+void addExclamation(char* ch);
+
+int characterFind(const char* ch, const char key);
+
+int compareString(const char* str1, const char* str2);
+
+void eraseCharacter(char* strIn, char* strDest, const char ch);
+
+void reverseString(char* str, size_t size);
+
+int numberOfWords(const char* str);
